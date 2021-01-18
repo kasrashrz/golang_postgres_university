4c5517d134395f8a27238a39f0148d568fc19261
KasTheDeveloper <kasra.shirazi.1234@gmail.com>
2021-01-18 20:42:14 +0330

    constructors are all done

--- Controllers/university_branch.go
+++ Controllers/university_branch.go
@@ -3,79 +3,25 @@ package Controllers
 import (
 	"github.com/gin-gonic/gin"
 	"iran.gitlab.medrick.games/medrick/server/go_lang/sample_postgres_project/models"
-	"net/http"
-	"gorm.io/gorm"
 )
 
-
+var branch models.UniversityBranch
 func FindUniversityBranch(ctx *gin.Context){
-	db := ctx.MustGet("db").(*gorm.DB)
-	var uni_branches []models.UniversityBranch
-	db.Find(&uni_branches)
-	ctx.JSON(http.StatusOK,gin.H{
-		"result" : uni_branches,
-	})
+	branch.READ(ctx)
 }
 
 func CreateUniversityBranch(ctx *gin.Context){
-	db := ctx.MustGet("db").(*gorm.DB)
-	var input models.UniversityBranch
-	if err := ctx.ShouldBindJSON(&input); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	 UniBranch := models.UniversityBranch{
-		 Name:         input.Name,
-		 Address:      input.Address,
-		 URL:          input.URL,
-		 CreationDate: input.CreationDate,
-		 UniversityID: input.UniversityID,
-	 }
-	for _, course := range input.Courses {
-		UniBranch.Courses = append(UniBranch.Courses, course)
-	}
-
-	db.Save(&UniBranch)
-	ctx.JSON(http.StatusOK,gin.H{
-		"data" : true,
-	})
+	branch.CREATE(ctx)
 }
 
 func UpdateUniversityBranch(ctx *gin.Context) {
-	var UniBranch models.UniversityBranch
-	db := ctx.MustGet("db").(*gorm.DB)
-	if err := db.Where("id = ?", ctx.Param("id")).First(&UniBranch).Error; err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
-		return
-	}
-	var input models.Teacher
-	if err := ctx.ShouldBindJSON(&input); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	db.Model(&UniBranch).Updates(input)
-	ctx.JSON(http.StatusOK, gin.H{"data": UniBranch})
+	branch.UPDATE(ctx)
 }
 
 func DeleteUniversityBranch(ctx *gin.Context){
-	db := ctx.MustGet("db").(gorm.DB)
-	var uni_branch models.UniversityBranch
-	if err := db.Where("id = ?", ctx.Param("id")).First(&uni_branch).Error; err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err})
-	}
-	db.Delete(&uni_branch)
-	ctx.JSON(http.StatusOK,gin.H{
-		"data":true,
-	})
+	branch.DELETE(ctx)
 }
 
 func FindStudentsByUniversityBranch(ctx *gin.Context) {
-	//var university models.University
-	var student []models.Student
-
-	db := ctx.MustGet("db").(*gorm.DB)
-	if err := db.Table("students").Select("*").Where("university_branch_id = ?", ctx.Param("id")).Scan(&student).Error; err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err})
-	}
-	ctx.JSON(http.StatusOK, gin.H{"data": student})
+	branch.StudentByUniBranch(ctx)
 }
--- models/university_branch.go
+++ models/university_branch.go
@@ -1,6 +1,11 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"net/http"
+)
 
 type UniversityBranch struct {
 	gorm.Model
@@ -12,24 +17,85 @@ type UniversityBranch struct {
 	University University `gorm:"references:ID"`
 	Courses      []Course `gorm:"many2many:UniversityBranch_courses"`
 }
-//
-//type CreateUniversityBranchInput struct {
-//	gorm.Model
-//	Name         string   `gorm:"unique;"json:"name"`
-//	Address      string   `json:"address"`
-//	URL 		 string	  `json:"url"`
-//	CreationDate string	  `json:"CreationDate"`
-//	UniversityID uint
-//	University University  `gorm:"references:ID"`
-//	Courses      []Course `gorm:"many2many:UniversityBranch_courses"`
-//}
-//
-//type UpdateUniversityBranchInput struct {
-//	gorm.Model
-//	Name         string   `gorm:"unique;"json:"name"`
-//	Address      string   `json:"address"`
-//	URL 		 string	  `json:"url"`
-//	CreationDate string	  `json:"CreationDate"`
-//	University University `gorm:"references:ID"`
-//	Courses      []Course `gorm:"many2many:UniversityBranch_courses"`
-//}
+
+type Uni_Branch_Handler interface {
+	CREATE(ctx *gin.Context)
+	READ(ctx *gin.Context)
+	UPDATE(ctx *gin.Context)
+	DELETE(ctx *gin.Context)
+	StudentByNameOrMail(ctx *gin.Context)
+}
+
+func (uni_branch *UniversityBranch) CREATE(ctx *gin.Context){
+	db := ctx.MustGet("db").(*gorm.DB)
+	var input UniversityBranch
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	UniBranch := UniversityBranch{
+		Name:         input.Name,
+		Address:      input.Address,
+		URL:          input.URL,
+		CreationDate: input.CreationDate,
+		UniversityID: input.UniversityID,
+	}
+	for _, course := range input.Courses {
+		UniBranch.Courses = append(UniBranch.Courses, course)
+	}
+
+	db.Save(&UniBranch)
+	ctx.JSON(http.StatusOK,gin.H{
+		"data" : true,
+	})
+	fmt.Println("CREATE INTERFACE")
+}
+func (uni_branch *UniversityBranch) READ(ctx *gin.Context){
+	db := ctx.MustGet("db").(*gorm.DB)
+	var uni_branches []UniversityBranch
+	db.Find(&uni_branches)
+	ctx.JSON(http.StatusOK,gin.H{
+		"result" : uni_branches,
+	})
+	fmt.Println("READ INTERFACE")
+}
+func (uni_branch UniversityBranch) UPDATE(ctx *gin.Context){
+	var UniBranch UniversityBranch
+	db := ctx.MustGet("db").(*gorm.DB)
+	if err := db.Where("id = ?", ctx.Param("id")).First(&UniBranch).Error; err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+	var input Teacher
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	db.Model(&UniBranch).Updates(input)
+	ctx.JSON(http.StatusOK, gin.H{"data": UniBranch})
+	fmt.Println("UPDATE INTERFACE")
+}
+func (branch *UniversityBranch) DELETE(ctx *gin.Context){
+	db := ctx.MustGet("db").(gorm.DB)
+	var uni_branch UniversityBranch
+	if err := db.Where("id = ?", ctx.Param("id")).First(&uni_branch).Error; err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err})
+	}
+	db.Delete(&uni_branch)
+	ctx.JSON(http.StatusOK,gin.H{
+		"data":true,
+	})
+
+	fmt.Println("DELETE INTERFACE")
+}
+func (uni_branch *UniversityBranch) StudentByUniBranch(ctx *gin.Context){
+	//var university models.University
+	var student []Student
+
+	db := ctx.MustGet("db").(*gorm.DB)
+	if err := db.Table("students").Select("*").Where("university_branch_id = ?", ctx.Param("id")).Scan(&student).Error; err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err})
+	}
+	ctx.JSON(http.StatusOK, gin.H{"data": student})
+	fmt.Println("STU API")
+}
